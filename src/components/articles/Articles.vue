8925150c335323ed32d0d9738ddfa0276d28c0b8
xhf <1041597213@qq.com>
2019-07-02 22:39:17 +0800

    element

--- src/components/articles/Articles.vue
+++ src/components/articles/Articles.vue
@@ -1,24 +1,32 @@
 <template>
   <div>
-    面试技巧
+    <el-button type="infor" class="newKill">新增面试技巧</el-button>
+    <el-input placeholder="请输入内容" v-model="input">
+      <template slot="prepend">关键字</template>
+    </el-input>
   </div>
 </template>
 
 <script>
 export default {
-
-  data () {
+  data() {
     return {
+      input: ''
     }
   },
 
   methods: {},
 
-  created () {
-  }
+  created() {}
 }
 </script>
 
 <style lang='less' scoped>
-
+.newKill {
+  width: 100px;
+  font-size: 12px;
+}
+.el-input-group .el-input-group__prepend {
+  width: 200px;
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -6,13 +6,8 @@ import Login from './components/dashboard/Login.vue'
 // 主页
 import Home from './components/dashboard/Home.vue'
 // 后台企业模块
-<<<<<<< HEAD
-import Users from './components/user/User.vue';
-// 面试技巧
-import Articles from './components/articles/Articles.vue'
-=======
 import Users from './components/user/User.vue'
->>>>>>> 46a3f0ec1a63ae50b8935c74f5dadd5894f9c3cb
+import Articles from './components/articles/Articles.vue'
 
 Vue.use(Router)
 
